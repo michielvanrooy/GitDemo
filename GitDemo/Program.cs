1a20845335d734236b6d70a49ab3ae08eefcbbb9
Michiel van Rooy <Michiel.vanRooy@standardbank.co.za>
2026-04-08 12:00:35 +0200

    changes

--- GitDemo/Program.cs
+++ GitDemo/Program.cs
@@ -1,2 +1,11 @@
 ﻿
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+v
+Console.WriteLine("Hello, World!");
+varConsole.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
